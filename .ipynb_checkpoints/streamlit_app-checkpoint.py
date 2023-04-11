89d210eef429e353812466e24c18af3079e52c99
unknown <louiecervantes@wvsu.edu.ph>
2023-04-11 16:22:03 +0800

    save changes

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -67,20 +67,9 @@ def app():
     st.subheader('The TextBlob Package')
     st.write('TextBlob is a Python package for natural language processing (NLP) tasks such as part-of-speech tagging, sentiment analysis, and text classification. It is built on top of the popular Natural Language Toolkit (NLTK) and provides a simple and intuitive API for performing various NLP tasks.')
     
-    st.subheader('Simple sentiment analysis task')
+    st.subheader('Sentiment Analysis')
     st.write("Sentiment analysis is the process of determining the emotional tone of a piece of text. TextBlob provides two properties for sentiment analysis: polarity and subjectivity. /n/nPolarity refers to the degree to which the text expresses a positive or negative sentiment. Polarity is represented as a float value between -1.0 and 1.0, where -1.0 represents a completely negative sentiment, 0.0 represents a neutral sentiment, and 1.0 represents a completely positive sentiment./n/nSubjectivity, on the other hand, refers to the degree to which the text expresses a subjective or objective viewpoint. Subjectivity is also represented as a float value between 0.0 and 1.0, where 0.0 represents a completely objective viewpoint and 1.0 represents a completely subjective viewpoint.")
     
-    st.write('Let us try the following statements.  Copy-paste the statement into the textbox and click the button to get the sentiment.')
-    st.write('He is a very good boy.  \
-              \nHe is not a good boy. \
-              \nEverybody says this man is poor.')
-    
-    user_input = st.text_input("Input the statement here:")
-    
-    if st.button('Submit'):  
-        result = TextBlob(user_input).sentiment
-        st.text(result)
-        
     st.subheader('Movie Review Dataset')
     st.write('We load a movie review dataset containing 2 columns: text - contains the text of the review, and label - contains the 0 for negative and 1 for positive reviews. The dataset contains 40,000 rows pf data. We load the first 20 rows for veiwing.')
     
