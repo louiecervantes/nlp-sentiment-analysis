925765bb87d60e7dd06ecee3bbfe035d82ac501d
unknown <louiecervantes@wvsu.edu.ph>
2023-04-11 13:20:20 +0800

    save changes

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -52,7 +52,7 @@ def app():
     if st.button('Load Dataset'):  
         df = pd.read_csv('TextBlobTrain.csv')
         st.write(df.head(20))
-        st.write(df.shape)
+        st.write('Dataset shape: ' + df.shape)
     
 # run the app
 if __name__ == "__main__":
